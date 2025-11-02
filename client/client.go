fbc304124ca9e6bb844932ed2fef30254b38c7e4
DakshBaxi <baxidaksh2004@gmail.com>
2025-11-03 00:39:11 +0530

    Created a chat server on tcp server with client connection

--- client/client.go
+++ client/client.go
@@ -1,24 +1,58 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "net"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-    conn, err := net.Dial("tcp", "localhost:8080")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        reader := bufio.NewReader(os.Stdin)
-        fmt.Print("Text to send: ")
-        text, _ := reader.ReadString('\n')
-        fmt.Fprintf(conn, text + "\n")
-        message, _ := bufio.NewReader(conn).ReadString('\n')
-        fmt.Print("Message from server: "+message)
-    }
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// 1. Get username from the user.
+	fmt.Print("Please enter your name: ")
+	nameReader := bufio.NewReader(os.Stdin)
+	username, err := nameReader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Failed to read username:", err)
+	}
+	username = strings.TrimSpace(username)
+
+	// 2. Send the username to the server as the first message.
+	fmt.Fprintln(conn, username)
+
+	// Start a goroutine to continuously read messages from the server
+	// and print them to the console.
+	go receiveMessages(conn)
+
+	// Use the main goroutine to read input from the user's keyboard
+	// and send it to the server.
+	fmt.Println("You are now connected. Type a message and press Enter.")
+	for {
+		msg, _ := nameReader.ReadString('\n')
+		text := strings.TrimSpace(msg)
+		_, err := fmt.Fprintln(conn, text)
+		if err != nil {
+			log.Println("Failed to send message:", err)
+			break
+		}
+	}
+
+}
+
+// receiveMessages runs in a separate goroutine to handle incoming messages.
+func receiveMessages(conn net.Conn) {
+	// Copy from the connection's reader to standard output (the console).
+	// This will block until the connection is closed or an error occurs.
+	if _, err := io.Copy(os.Stdout, conn); err != nil {
+		log.Println("Connection lost. ", err)
+	}
 }
--- server.go
+++ server.go
@@ -1,58 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"strings"
-	"time"
+	"sync"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 	// establishing server
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	fmt.Print("Listening on port 8080 \n")
 	// establishing connection to the server
-
+	// Using sync.Map to not deal with concurrency slice/map issues
+	var connMap = &sync.Map{}
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
+		// storing each conn in map
+		id := uuid.New().String()
+		connMap.Store(id, conn)
+
 		// ip address fetching
 		remoteAddr := conn.RemoteAddr().String()
 		host, _, err := net.SplitHostPort(remoteAddr)
-	if err != nil {
-		log.Printf("Error splitting host and port: %v\n", err)
-		return
-	}
-	fmt.Printf("Client IP address: %s\n", host)
-		go handleConn(conn)
+		if err != nil {
+			log.Printf("Error splitting host and port: %v\n", err)
+			return
+		}
+		fmt.Printf("Client IP address: %s\n", host)
+		go handleConn(conn, id, connMap)
 	}
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	buff := make([]byte, 1024)
-	// Get the remote address (client's IP and port)
-	
-	for {
-		
-		n, err := conn.Read(buff)	// returns no. of byte
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-		fmt.Printf("Recieved:%s", string(buff[:n])) 
-		data := strings.ToUpper(string(buff[:n]))
-		_, err = conn.Write([]byte(data))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		conn.SetDeadline(time.Now().Add(10 * time.Second))
+func handleConn(conn net.Conn, id string, connMap *sync.Map) {
+	// First, read the username from the connection
+	username, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Println("Failed to read username:", err)
+		conn.Close()
+		connMap.Delete(id)
+		return
 	}
+	username = strings.TrimSpace(username)
+
+	// Announce that a new user has joined
+	broadcastMessage(fmt.Sprintf("--- %s has joined the chat ---\n", username), connMap, nil)
+
+	defer func() {
+		conn.Close()
+		connMap.Delete(id)
+		// Announce that the user has left
+		broadcastMessage(fmt.Sprintf("--- %s has left the chat ---\n", username), connMap, nil)
+	}()
+
+	for {
+		userInput, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			// This error usually means the client has disconnected.
+			// The defer function will handle cleanup and announcement.
+			return
+		}
+
+		// Prepend username to the message and broadcast it
+		message := fmt.Sprintf("%s: %s", username, userInput)
+		broadcastMessage(message, connMap, conn)
+	}
+}
+
+// broadcastMessage sends a message to all clients in the connMap.
+// If sender is not nil, it skips sending the message back to the original sender.
+func broadcastMessage(message string, connMap *sync.Map, sender net.Conn) {
+	connMap.Range(func(key, value interface{}) bool {
+		if targetConn, ok := value.(net.Conn); ok {
+			// Don't send the message back to the sender
+			if targetConn != sender {
+				_, err := targetConn.Write([]byte(message))
+				if err != nil {
+					log.Println("Removing disconnected client:", err)
+					targetConn.Close()
+					connMap.Delete(key)
+				}
+			}
+		}
+		return true
+	})
 }
